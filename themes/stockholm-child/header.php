8c799f8ffa73a1a3494969947c62e5ebeea00190
surreal8 <tinashah8@gmail.com>
2016-04-03 23:24:32 -0500

    header and homepage changes

--- themes/stockholm-child/header.php
+++ themes/stockholm-child/header.php
@@ -381,8 +381,13 @@ global $qode_toolbar;
 							if (isset($qode_options['logo_image_popup']) && $qode_options['logo_image_popup'] != ""){ $logo_image_popup = $qode_options['logo_image_popup'];}else{ $logo_image_popup =  get_template_directory_uri().'/img/logo_white.png'; };
                             if (isset($qode_options['logo_image_fixed_hidden']) && $qode_options['logo_image_fixed_hidden'] != ""){ $logo_image_fixed_hidden = $qode_options['logo_image_fixed_hidden'];}else{ $logo_image_fixed_hidden =  get_template_directory_uri().'/img/logo.png'; };
 							?>
-							<div class="q_logo"><a href="<?php echo home_url('/'); ?>"><img class="normal" src="<?php echo $logo_image; ?>" alt="Logo"/><img class="light" src="<?php echo $logo_image_light; ?>" alt="Logo"/><img class="dark" src="<?php echo $logo_image_dark; ?>" alt="Logo"/><img class="sticky" src="<?php echo $logo_image_sticky; ?>" alt="Logo"/><?php if($enable_popup_menu == 'yes'){ ?><img class="popup" src="<?php echo $logo_image_popup; ?>" alt="Logo"/><?php } ?></a></div>
-                            <div class="sitetitle"><a href="<?php echo home_url('/'); ?>">The Alfred Stieglitz Collection &nbsp; |  &nbsp; Photographs</a></div>
+							<div class="q_logo"><a href="<?php echo home_url('/'); ?>"><img class="normal" src="<?php echo $logo_image; ?>" alt="Logo"/><img class="light" src="<?php echo $logo_image_light; ?>" alt="Logo"/><img class="dark" src="<?php echo $logo_image_dark; ?>" alt="Logo"/><img class="sticky" src="<?php echo $logo_image_sticky; ?>" alt="Logo"/><?php if($enable_popup_menu == 'yes'){ ?><img class="popup" src="<?php echo $logo_image_popup; ?>" alt="Logo"/><?php } ?></a></div>		
+                            <?php if ( is_page( 'Home Default' )  ) {?>
+								<div class="sitetitlehome"><a href="<?php echo home_url('/'); ?>">The Alfred Stieglitz Collection</a></div>
+							<?php } else { ?>
+								<div class="sitetitle"><a href="<?php echo home_url('/'); ?>">The Alfred Stieglitz Collection</a></div>
+							<?php } ?>
+                            
                             <?php if($header_bottom_appearance == "fixed_hiding") { ?>
                                 <div class="q_logo_hidden"><a href="<?php echo home_url('/'); ?>"><img alt="Logo" src="<?php echo $logo_image_fixed_hidden; ?>" style="height: 100%;"></a></div>
                             <?php } ?>
